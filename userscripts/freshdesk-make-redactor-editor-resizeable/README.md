7db5ec9da58ef4c4248132e53486af4d0c1583f3
Glenn 'devalias' Grant <glenn@devalias.net>
2025-08-24 15:29:46 +1000

    [userscript] add freshdesk-make-redactor-editor-resizeable

---
+++ userscripts/freshdesk-make-redactor-editor-resizeable/README.md
@@ -0,0 +1,18 @@
+# Freshdesk Make 'Redactor Editor' Resizeable
+
+```javascript
+// ==UserScript==
+// @name          Freshdesk Make 'Redactor Editor' Resizeable
+// @description   Make Freshdesk 'Redactor Editor' (used when writing ticket descriptions) resizable vertically via CSS rule injection
+// @author        Glenn 'devalias' Grant (devalias.net)
+// @homepageURL   https://github.com/0xdevalias/userscripts
+// @supportURL    https://github.com/0xdevalias/userscripts/issues
+// @downloadURL   https://github.com/0xdevalias/userscripts/raw/main/userscripts/freshdesk-make-redactor-editor-resizeable/freshdesk-make-redactor-editor-resizeable.user.js
+// @namespace     https://www.devalias.net/
+// @version       0.1
+// @match         https://*.freshdesk.com/*/support/tickets/*
+// @grant         none
+// ==/UserScript==
+```
+
+- [Install](https://github.com/0xdevalias/userscripts/raw/main/userscripts/freshdesk-make-redactor-editor-resizeable/freshdesk-make-redactor-editor-resizeable.user.js)
---
+++ userscripts/freshdesk-make-redactor-editor-resizeable/freshdesk-make-redactor-editor-resizeable.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name          Freshdesk Make 'Redactor Editor' Resizeable
+// @description   Make Freshdesk 'Redactor Editor' (used when writing ticket descriptions) resizable vertically via CSS rule injection
+// @author        Glenn 'devalias' Grant (devalias.net)
+// @homepageURL   https://github.com/0xdevalias/userscripts
+// @supportURL    https://github.com/0xdevalias/userscripts/issues
+// @downloadURL   https://github.com/0xdevalias/userscripts/raw/main/userscripts/freshdesk-make-redactor-editor-resizeable/freshdesk-make-redactor-editor-resizeable.user.js
+// @namespace     https://www.devalias.net/
+// @version       0.1
+// @match         https://*.freshdesk.com/*/support/tickets/*
+// @grant         none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const css = `
+    .redactor_box > .redactor_editor { resize: vertical !important; }
+  `;
+
+  const style = document.createElement('style');
+  style.textContent = css;
+  document.head.appendChild(style);
+})();
